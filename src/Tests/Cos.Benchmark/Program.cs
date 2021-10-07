a908d1881a02d2e5cf10096926a6bb5f0d817cdb
Henning Hultman Bjørgo <henning.bjorgo@easee.no>
2021-10-07 12:03:46 +0200

    Add benchmark project

---
+++ src/Tests/Cos.Benchmark/Program.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Easee.Cos;
+using System;
+using System.Collections.Generic;
+
+namespace Cos.Benchmark
+{
+    [MemoryDiagnoser]
+    public class CosDeserialize
+    {
+        private readonly CosReader _cosReader;
+        readonly byte[] _oneObservation = Convert.FromBase64String("ASAAAWCVLGMAATB4QS1gQg==");
+        readonly byte[] _tenObservations = Convert.FromBase64String("ASAAAWCVK4wACjB4AAAAADC2PffO2TC3PffO2TDKQ10t0zDLQ1y0OTDMQ1Z9sjDNQ1bcKUBtAAAABMBkAAFCQC4AAAAX");
+
+        public CosDeserialize()
+        {
+            _cosReader = new();
+        }
+
+        [Benchmark]
+        public List<Observation> DeserializePackageWithOneObservation() =>
+            _cosReader.Deserialize(_oneObservation);
+
+        [Benchmark]
+        public List<Observation> DeserializePackageWithTenObservations() =>
+            _cosReader.Deserialize(_tenObservations);
+    }
+
+    /// <summary>
+    /// Run this to get a benchmark of the performance (run time and memory allocations)
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<CosDeserialize>();
+        }
+    }
+}
