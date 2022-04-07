1368a0d23c760808ae033a875946e333761c47ab
Jan Erik Foss <jan.erik.foss@webstep.no>
2022-04-07 23:33:21 +0200

    Extract enums into their own file

--- src/Cos/CosReader.cs
+++ src/Cos/CosReader.cs
@@ -153,29 +153,5 @@ namespace Easee.Cos
                     throw new ArgumentException($"Unsupported observation type value: {observationType}.");
             }
         }
-
-        private enum COSHeaderFlag : byte
-        {
-            COS_HEADER_MULTI_OBSERVATIONS = 0x20,
-            COS_HEADER_MULTI_TIMESTAMPS = 0x40,
-            COS_HEADER_64BIT_TIMESTAMPS = 0x80
-        }
-
-        private enum COSObservationType : byte
-        {
-            COS_OBS_TYPE_UNDEFINED = 0,
-            COS_OBS_TYPE_BOOLEAN = 1,
-            COS_OBS_TYPE_DOUBLE = 2,
-            COS_OBS_TYPE_FLOAT = 3,
-            COS_OBS_TYPE_INT32 = 4,
-            COS_OBS_TYPE_INT16 = 5,
-            COS_OBS_TYPE_UINT16 = 6,
-            COS_OBS_TYPE_INT8 = 7,
-            COS_OBS_TYPE_UINT8 = 8,
-            COS_OBS_TYPE_POSITION_2D = 9,
-            COS_OBS_TYPE_POSITION_3D = 10,
-            COS_OBS_TYPE_ASCII = 11,
-            COS_OBS_TYPE_UTF8 = 12
-        }
     }
 }
