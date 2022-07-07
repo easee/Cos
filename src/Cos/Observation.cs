00280fafbc32f420fd720ba57d113151b8b0dc0e
Jan Erik Foss <jan.erik.foss@webstep.no>
2022-07-07 17:05:14 +0200

    Add ObservationType property to base Observation class

--- src/Cos/Observation.cs
+++ src/Cos/Observation.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using Easee.Cos.Exceptions;
 
 namespace Easee.Cos
 {
     public abstract class Observation : IComparable<Observation>
     {
-        public Observation(int observationId, DateTime timestamp)
+        public Observation(int observationId, DateTime timestamp, ObservationType type)
         {
             ObservationId = observationId;
+            Type = type;
             Timestamp = timestamp;
         }
 
         public int ObservationId { get; }
+        public ObservationType Type { get; }
         public DateTime Timestamp { get; }
 
         public override string ToString() => $"ID:{ObservationId}.";
@@ -22,7 +25,14 @@ namespace Easee.Cos
     public class Observation<TValue> : Observation
     {
         public Observation(int observationId, DateTime timestamp, TValue value)
-            : base(observationId, timestamp)
+            : base(observationId, timestamp, typeof(TValue) switch { 
+                Type i when i == typeof(int) => ObservationType.Integer,
+                Type d when d == typeof(double) => ObservationType.Double,
+                Type b when b == typeof(bool) => ObservationType.Boolean,
+                Type s when s == typeof(string) => ObservationType.String,
+                Type p when p == typeof(Position) => ObservationType.Position,
+                _ => throw new UnsupportedObservationTypeException(typeof(TValue).FullName),
+            })
         {
             Value = value;
         }
@@ -33,6 +43,7 @@ namespace Easee.Cos
         public override int CompareTo(Observation? other) {
             return (other is Observation<TValue> o
                 && o.ObservationId == ObservationId
+                && o.Type == Type
                 && o.Timestamp == Timestamp
                 && Comparer<TValue>.Default.Compare(Value, o.Value) == 0) 
                 ? 0 : 1;
---
+++ src/Tests/Cos.Tests/ObservationTests.cs
@@ -0,0 +1,44 @@
+﻿using Easee.Cos;
+using System;
+using Xunit;
+
+namespace Cos.Tests
+{
+    public class ObservationTests
+    {
+        [Fact]
+        public void Boolean_type()
+        {
+            var observation = new Observation<bool>(150, DateTime.UtcNow, true);
+            Assert.Equal(ObservationType.Boolean, observation.Type);
+        }
+
+        [Fact]
+        public void Double_type()
+        {
+            var observation = new Observation<double>(150, DateTime.UtcNow, 1.234);
+            Assert.Equal(ObservationType.Double, observation.Type);
+        }
+
+        [Fact]
+        public void Integer_type()
+        {
+            var observation = new Observation<int>(150, DateTime.UtcNow, 12);
+            Assert.Equal(ObservationType.Integer, observation.Type);
+        }
+
+        [Fact]
+        public void String_type()
+        {
+            var observation = new Observation<string>(150, DateTime.UtcNow, "hello world");
+            Assert.Equal(ObservationType.String, observation.Type);
+        }
+
+        [Fact]
+        public void Position_type()
+        {
+            var observation = new Observation<Position>(150, DateTime.UtcNow, new(59, 10));
+            Assert.Equal(ObservationType.Position, observation.Type);
+        }
+    }
+}
